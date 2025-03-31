1ba3aeab5dbadea9725bdc216e0e3013c43be0d5
louisjohns32 <louis2johnston3@gmail.com>
2025-03-31 16:09:41 +0100

    set up hateoas and orderbook model assembler

--- exchange/src/main/java/org/louisjohns32/personal/exchange/ExchangeApplication.java
+++ exchange/src/main/java/org/louisjohns32/personal/exchange/ExchangeApplication.java
@@ -2,8 +2,9 @@ package org.louisjohns32.personal.exchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class ExchangeApplication {
 
 	public static void main(String[] args) {
