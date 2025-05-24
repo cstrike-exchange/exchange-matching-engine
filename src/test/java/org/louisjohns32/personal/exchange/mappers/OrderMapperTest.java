1c8deff17b7afce8385b2fb6db9f36e6d87859a4
louisjohns32 <louis.johnston@student.manchester.ac.uk>
2025-05-24 18:06:40 +0100

    order request dto and mapper

---
+++ src/test/java/org/louisjohns32/personal/exchange/mappers/OrderMapperTest.java
@@ -0,0 +1,36 @@
+package org.louisjohns32.personal.exchange.mappers;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.louisjohns32.personal.exchange.constants.Side;
+import org.louisjohns32.personal.exchange.dto.OrderRequestDTO;
+import org.louisjohns32.personal.exchange.entities.Order;
+
+public class OrderMapperTest {
+
+    private final OrderMapper mapper = new OrderMapper();
+
+    @Test
+    void toEntity_shouldMapAllFieldsCorrectly() {
+        OrderRequestDTO dto = new OrderRequestDTO(100.0, 200.0, Side.BUY, "AAPL");
+
+        Order order = mapper.toEntity(dto);
+
+        assertEquals(0, order.getId());
+        assertEquals(dto.getQuantity(), order.getQuantity());
+        assertEquals(dto.getPrice(), order.getPrice());
+        assertEquals(dto.getSide(), order.getSide());
+        assertEquals(0.0, order.getFilledQuantity());
+    }
+
+    @Test
+    void toEntity_shouldHandleSELLSideCorrectly() {
+        OrderRequestDTO dto = new OrderRequestDTO(10.0, 500.0, Side.SELL, "GOOG");
+        Order order = mapper.toEntity(dto);
+
+        assertEquals(Side.SELL, order.getSide());
+        assertEquals(10.0, order.getQuantity());
+        assertEquals(500.0, order.getPrice());
+    }
+}
